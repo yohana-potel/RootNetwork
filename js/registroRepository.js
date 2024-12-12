828afc22a094317b31875fc00d96790f7f74ce5c
Andrea Suarez <andrea.suarez@itscordoba.edu.ar>
2024-12-12 11:09:28 -0300

    creada funcion para la conexion del backend y front end

---
+++ js/registroRepository.js
@@ -0,0 +1,23 @@
+
+function getRegister(peticion, success) {
+    console.log(peticion);
+    fetch('http://localhost:5156/registro', {
+        method: 'POST',
+        body: JSON.stringify(peticion),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    })
+    .then((response) => {
+       
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then((json) => success(json))
+    .catch((e) => {
+        console.error('Error during regitrar request:', e);
+        
+    });
+}
