ea760c749292f5398f3fdf719bab99eda587a7fc
Andrea Suarez <andrea.suarez@itscordoba.edu.ar>
2025-03-06 10:09:28 -0300

    creado boton para redireccionar al registro

---
+++ js/botonCondiciones.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("rootnetwork").addEventListener("click", function() {
+        window.location.href = "registro.html"; // Cambia esto por la URL real de la página de registro
+    });
+});
